8d984ed8cf9ea8e21fb5552bdd8f75ddb8ae5b4c
majo06002 <Max-Lukas@outlook.com>
2023-09-20 09:57:24 +0200

    Add project files.

---
+++ inlämnings uppgift 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace inlämningsuppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok");
+            string bok=Console.ReadLine();
+            Console.WriteLine("Din favortit bok är " + bok);
+        }
+    }
+}
